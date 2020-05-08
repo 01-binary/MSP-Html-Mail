30503bab15b5012522a58aaac67e35481098254b
dgujs <binary01@kakao.com>
2020-05-08 10:18:31 +0900

    end

--- input/js/main.js
+++ input/js/main.js
@@ -2,10 +2,6 @@
 (function ($) {
     "use strict";
 
-
-
-  
-  
     /*==================================================================
     [ Validate ]*/
     var input = $('.validate-input .input100');
